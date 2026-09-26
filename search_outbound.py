6bb8d902cf39c047bae45037b4add36793b51a3c
sonufgupta <66611436+sonufgupta@users.noreply.github.com>
2026-09-26 12:02:47 +0530

    Delete search_outbound.py

--- search_outbound.py
+++
@@ -1,6 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-for idx, line in enumerate(lines):
-    if 'outboundactive' in line.lower() or 'sessionoutbound' in line.lower() or 'outboundworkstation' in line.lower():
-        print(f"{idx+1}: {line.strip()}")
